ae3314455650f9406516aeff42a8fa4c8b7b3ad8
DeMarc Johnson <demarcjohnson@gmail.com>
2024-01-01 20:28:51 -0500

    added plugin-react package

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,24 @@
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths';
 import reactRefresh from "@vitejs/plugin-react-refresh"; 
+import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [reactRefresh(), tsconfigPaths()],
-  resolve: {
-    alias: {
-      src: path.resolve('src/'),
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react(), reactRefresh(), tsconfigPaths()],
+    resolve: {
+      alias: {
+        src: path.resolve('src/'),
+      },
     },
-  },
-  base: '/css-composer-2/'
+    base: '/'
+  }
+
+  if (command !== 'serve') {
+    config.base = '/css-composer-2/'
+  }
+
+  return config;
 })
